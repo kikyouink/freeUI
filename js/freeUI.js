c12000dddad562d2f15831c4943a21c2adee9118
Yuan Tan <2578443177@qq.com>
2018-01-04 18:36:46 +0800

    简化代码

--- js/freeUI.js
+++ js/freeUI.js
@@ -1,28 +1,26 @@
-	var type;
-	var source;
-	
-	function allowDrop(ev) {
-		ev.preventDefault();
-	
-		source = ev.target;
-		//	console.log(source);
-	
-	}
-	
-	function drag(ev) {
-		type = ev.target.className.split(' ')[1];
-		console.log(type);
-	}
-	
-	function drop(ev) {
-		console.log(ev.target + 'drop');
-		ev.preventDefault();
-		if(type == 'div') {
-			var div = $(source).putDiv('default', '示例文字DIV');
-			div.putDiv('coor');
-		} else var node = $(source).put(type, 'default');
-	
-	}
+var type;
+var source;
+
+function allowDrop(ev) {
+	ev.preventDefault();
+	source = ev.target;
+}
+
+function drag(ev) {
+	type = ev.target.className.split(' ')[1];
+	console.log(type);
+}
+
+function drop(ev) {
+	console.log(ev.target + 'drop');
+	ev.preventDefault();
+	if(type == 'div') {
+		var div = $(source).putDiv('default', '示例文字DIV');
+		div.putDiv('coor');
+	} else
+		var node = $(source).put(type, 'default');
+
+}
 (function() {
 	'use strict'
 	//----------------数组方法--------------------
@@ -44,21 +42,14 @@
 	//		return null;
 	//	}
 	var moving;
-	function print(str) {
-		$(".command_input").val(str);
-	};
-
-	function run() {
-		var text = $('.command_input').val();
-		$('.command_input').val('');
-		eval(text);
-	}
 	//----------------JQ扩展--------------------	
 	$.fn.extend({
+		print: function(str) {
+			$(this).text(str)
+		},
 		put: function(type, className, innerHTML) {
 			if(!className) className = '';
 			if(!innerHTML) innerHTML = '';
-			console.log(type);
 			var node = $("<" + type + "></" + type + ">");
 			node.addClass(className);
 			node.text(innerHTML);
@@ -69,79 +60,66 @@
 			if(!className) className = '';
 			if(!innerHTML) innerHTML = '';
 			var div = $("<div></div>");
-			div.addClass(className);			
+			div.addClass(className);
 			div.text(innerHTML);
 			$(this).append(div);
 			return div;
 		},
-		slideDown:function(){
-			moving=true;
-			$(this).css({
-				'z-index': 100,
-				'background-color': '#FDF0CC',
-				'border':'2px solid gray',
-			});
-			$(this).animate({
-				height: "120px"
-			},function(){
-				moving=false;
-			});
+		putInput: function(val) {
+			var input = $(this).put('input');
+			input.attr('type', 'number');
+			input.val(val);
 		},
-		slideUp:function(){
-			moving=true;
-			$(this).animate({
-				height: "0px"
-			},function(){
-				$(this).css({
-					'z-index':1,
-					'border':'none',
-				});
-				moving=false;
-			});
+		putColor: function() {
+			var input = $(this).put('input');
+			input.attr('type', 'color');
 		},
-		putParam:function(area){
-			function a(){
-				var input=menu.put('input');
-				input.attr('type','number');
-				input.val(area[i]);
+		putMenu: function(array) {
+			var select_box = $(this).putDiv('select_box');
+			var select = select_box.putDiv('select', 'default');
+			var ul = select.put('ul', 'option');
+			for(var i = 0; i < array.length; i++) {
+				var option = ul.put('li', '', array[i]);
 			}
-			function aa(){
-				
+		},
+		getParent: function(num) {
+			var parent;
+			while(num--) {
+				if(parent) parent = parent.parent();
+				else parent = $(this).parent();
 			}
-			function b(){
-				var input=menu.put('input');
-				input.attr('type','color');
-			}
-			function c(){
-				var select_box=menu.putDiv('select_box');
-				var select=select_box.putDiv('select','default');
-				var ul=select.put('ul','option');
-				for(var j=0;j<area[i].length;j++){
-					var option=ul.put('li','',area[i][j]);
-				}
-			}
-			for(var i in area){
-				var menu=$(this).putDiv('menu '+i,i);
-				if(typeof area[i]=='number'){
-					a();
-				}
-				else if(typeof area[i]=='string'){
-					b();
-				}
-				else{
-					var first=area[i][0];
-					if(first!='color'&&typeof first=='string'){
-						c();
-					}
-					else{
-						for(var j=0;j<area[i].length;j++){
-							if(typeof area[i][j]=='number') aa();
-							else if(typeof area[i][j]=='string') bb();
-							else cc();
+			return parent;
+		},
+		putParam: function(area) {
+			for(var i in area) {
+				if($.type(area[i]) == 'object') {
+					console.log(area[i]);
+					var menul = $(this).putDiv('menul');
+					var menu = menul.putDiv('menu ' + i, i);
+					var fold = menul.putDiv('fold');
+					for(var j in area[i]) {
+						if($.type(area[i][j]) == 'number') {
+							var menuItem = fold.putDiv('menu item ' + j, j);
+							menuItem.putInput(area[i][j]);
+						} else if(area[i][j] == 'color') {
+							var menuItem = fold.putDiv('menu item ' + j, j);
+							menuItem.putColor();
+						} else {
+							var menuItem = fold.putDiv('menu item ' + j, j);
+							menuItem.putMenu(area[i][j]);
 						}
 					}
-					
+				} else {
+					var menu = $(this).putDiv('menu ' + i, i);
+					if($.type(area[i]) == 'number') {
+						menu.putInput(area[i]);
+					} else if(area[i] == 'color') {
+						menu.putColor();
+					} else if($.type(area[i]) == 'array') {
+						menu.putMenu(area[i]);
+					}
 				}
+
 			}
 		}
 	});
@@ -161,7 +139,7 @@
 			'ondragover': 'allowDrop(event)'
 		});
 		$('.code').attr('contenteditable', 'true');
-		$('.menu.item').hide();
+		//		$('.menu.item').hide();
 		$('.tab_item').click(function() {
 			$(this).addClass('active');
 			$(this).siblings().removeClass('active');
@@ -207,14 +185,16 @@
 			}
 		});
 		$('.front').on('mousedown', '.default', function(e) {
+			//右键清除目标
 			if(e.which == 3) {
 				$(this).fadeOut('slow', function() {
 					$(this).remove();
 				});
 				return false;
 			}
+			$('.default.active').removeClass('active');
+			$(this).addClass('active');
 			var $box = $(this);
-			$box.focus();
 			var position = $box.position();
 			this.posix = {
 				'x': e.pageX - position.left,
@@ -244,18 +224,18 @@
 			});
 			return false;
 		})
-		$('.default').click(function() {
-			$('.default.active').removeClass('active');
-			$(this).toggleClass('active');
-			return false;
-		});
 		//控制台
+		function run() {
+			var text = $('.command_input').val();
+			$('.command_input').val('');
+			eval(text);
+		}
 		$('.command_input').keydown(function(e) {
 			if(e.which == 13) run();
-		})
+		});
 		$('.command_btn').click(run);
 
-		//翻转查看代码
+		//双击翻转查看代码
 		$('.front').dblclick(function(e) {
 			var ss = $('.front').html();
 			//HTML标签转义
@@ -281,58 +261,48 @@
 		var param;
 		$('.basic').putParam(style_basic);
 		$('.advance').putParam(style_advance);
-		$('.option').prepend( $("<li>default</li>"));
-		$('.fold').on('click', function(e) {
-			$(this).siblings('.menu.item').slideToggle('fast');
+		//彻底解决冒泡问题
+		$('*').on('click', function(e) {
 			return false;
 		})
-		$('.select_box').click(function(){
-			$(this).find('.option').slideDown();
-		});
-		$('input').on('input porpertychange',function(){
-			var number=str.replace(/[^1-9]/g, '');//123
-			var string=str.replace(/\d+/g, '');//abc
+		$('.menu').on('click', function(e) {
+			$(this).siblings('.fold').slideToggle();
+		})
+		//Toggle下拉菜单
+		var slide='down';
+		$('.select').click(function() {
+			if(slide=='down'){
+				$(this).find('ul').slideDown();
+				slide='up';
+			}
+			else{
+				$(this).find('ul').slideUp();
+				slide='down';
+			}
 		});
 		$('li').hover(function() {
-			if(moving) return ;
-			var active=$('.default.active');
-			if(!active.length) return ;
-			var menu = $(this).parent().parent().parent();
-			var select_box=$(this).parent().parent();
-			var select = $(this).parent().siblings('.select');
-			//获取可更改的样式数量并建立数组，方便之后替换样式
-			var size=menu.children().size();
-			console.log('size:'+size);
-			var style=new Array(size);
-			var index=select_box.index();
-			console.log('index:'+index);			
+			if(moving) return;
+			var active = $('.default.active');
+			if(!active.length) return;
+			var menu = $(this).getParent(4);
+			var select_box = $(this).getParent(3);
+			var select = $(this).getParent(2);
+			var type = menu.attr('class').split(' ')[0];
 			var name = menu.attr('class').split(' ')[1];
-			var value = $(this).text();
-			select.attr('value', value);
-			select.text(value);
-			if(value == 'default') value = ' ';
-			var css = '';
-			menu.children().each(function() {
-				if($(this)[0].tagName == 'INPUT') {
-					var val = $(this).val();
-					if(val == '') val = 15;
-					css += val + 'px ';
-				}
-				if($(this)[0].tagName == 'DIV'){
-					css += value + ' ';
-				}
-			});
-			console.log(css);
-			$('.default.active').css(name, css);
-			return false;
-		})
+			var value=$(this).text();
+			console.log(value);
+			if(type=='menu'){
+				active.css(name,value);
+			}
+			else{
+				var style='';
+				console.log('fold');
+			}
+		});
 		$('li').click(function() {
-			var ul=$(this).parent();
+			var ul = $(this).parent();
 			ul.slideUp();
-			return false;	
-		})
-		$('.menu').click(function(){
-			$('.fold').slideToggle();
+			return false;
 		})
 	});
 }());
--- js/param.js
+++ js/param.js
@@ -1,23 +1,34 @@
 var style_basic={
 	display:['block','inline','none'],
 	position:['static','relative','absolute','fixed'],
-	font:[15,['Microsoft Yahei','Sans-serif','Serif']],
+	font:{
+		fontSize:15,
+		fontFamily:['Microsoft Yahei','Sans-serif','Serif'],
+	},
 	color:'color',
 	lineHeight:15,
 	textAligin:['left','center','right'],
 	
-}
+};
 var style_advance={
 	width:200,
 	height:400,
 	margin:0,
 	padding:0,
 	float:['none','left','right'],
-	border:[0,['solid','double','groove'],'color'],
-	background:['color',['cover','contain'],'src'],
+	border:{
+		borderWidth:0,
+		borderStyle:['solid','double','groove'],
+		boderColor:'color',
+	},
+	background:{
+		backgroundColor:'color',
+		backgroundSize:['cover','contain'],
+		backgroundImage:'src',
+	},
 	boxShadow:0,
 	borderRadius:0,	
-}
+};
 var style_flex={
 	flexDirection:['row','row-reverse','column','column-reverse'],
 	flexWrap:['nowrap','wrap'],
